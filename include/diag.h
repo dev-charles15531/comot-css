e59fd99dc1cc4a2da75ecc8361ca7bcc2a7d96db
dev-charles1551 <kingcharles15531@gmail.com>
2025-05-11 06:27:07 +0100

    chore: defined headers

---
+++ include/diag.h
@@ -0,0 +1,17 @@
+#ifndef DIAG_H
+#define DIAG_H
+
+#include "error.h"
+/*#include <stddef.h>*/
+
+// Max errors collected per session
+#define MAX_LEXER_ERRORS 32
+
+typedef struct {
+    LexerError errors[MAX_LEXER_ERRORS];
+    size_t count;
+} LexerDiagnostics;
+
+void lexer_diag_add(LexerDiagnostics* diag, LexerErrorCode code, size_t line, size_t column);
+
+#endif
---
+++ include/error.h
@@ -0,0 +1,26 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <stddef.h>
+
+// Lightweight error code enum
+typedef enum {
+    LEXER_OK = 0,
+    LEXER_ERR_UNTERMINATED_STRING,
+    LEXER_ERR_INVALID_ESCAPE,
+    LEXER_ERR_UNEXPECTED_CHAR,
+    LEXER_ERR_UNTERMINATED_COMMENT,
+    LEXER_ERR_UNKNOWN_TOKEN,
+    LEXER_ERR_BAD_NUMBER
+} LexerErrorCode;
+
+// Error structure
+typedef struct {
+    LexerErrorCode code;
+    size_t line;
+    size_t column;
+} LexerError;
+
+const char* lexer_error_message(LexerErrorCode code);
+
+#endif
